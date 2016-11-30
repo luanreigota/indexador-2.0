447b1eba8336dccfb8ddf0690d6ca0e305c5eab5
luan <luan@luan-H61M-DS2>
2016-11-30 17:12:21 -0200

    criacao da classe arquivo

---
+++ src/main/java/entity/Arquivo.java
@@ -0,0 +1,10 @@
+package entity;
+
+public class Arquivo {
+	
+	private String nome;
+	private String extensao;
+	private String caminho;
+	private String ultimaModificacao;
+	
+}
--- src/main/java/main/MainTeste.java
+++ src/main/java/main/MainTeste.java
@@ -7,17 +7,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
 
 
 public class MainTeste {
 
 	public static void main(String[] args) throws IOException {
 		
-		List<String> tipos = new ArrayList<>();
-		List<String> nomes = new ArrayList<>();
 		
 		Path path = Paths.get(System.getProperty("user.home")+"/bkp-portal-cmrj");
 		Files.walkFileTree(path, new SimpleFileVisitor<Path>(){
@@ -32,20 +28,9 @@ public class MainTeste {
 			@Override
 			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
 				// TODO Auto-generated method stub
-				System.out.println("arquivo: "+file.toString());
-				System.out.println("mime type: "+Files.probeContentType(file));
-				String tipo = Files.probeContentType(file);
-				if(!tipos.contains(tipo)){
-					tipos.add(Files.probeContentType(file));
-					nomes.add(file.toString());
-				}
 				return super.visitFile(file, attrs);
 			}
 		});
-		System.out.println("================================================");
-		for (int i=0; i<tipos.size(); i++) {
-			System.out.println(tipos.get(i)+"						| "+nomes.get(i));
-		}
 		
 	}
 }
