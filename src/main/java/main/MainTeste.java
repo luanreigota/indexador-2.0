6b8717813b9a6cd87e3897c17b77f932af538bee
luan <luan@luan-H61M-DS2>
2016-11-30 16:21:16 -0200

    verificando tipos de arquivo

--- src/main/java/main/MainTeste.java
+++ src/main/java/main/MainTeste.java
@@ -1,10 +1,53 @@
 package main;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 public class MainTeste {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		List<String> tipos = new ArrayList<>();
+		List<String> nomes = new ArrayList<>();
 		
+		Path path = Paths.get(System.getProperty("user.home")+"/bkp-portal-cmrj");
+		Files.walkFileTree(path, new SimpleFileVisitor<Path>(){
+		
+			@Override
+			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+				// TODO Auto-generated method stub
+				System.out.println("pasta: "+ dir.toString());
+				return super.postVisitDirectory(dir, exc);
+			}
+			
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				// TODO Auto-generated method stub
+				System.out.println("arquivo: "+file.toString());
+				System.out.println("mime type: "+Files.probeContentType(file));
+				String tipo = Files.probeContentType(file);
+				if(!tipos.contains(tipo)){
+					tipos.add(Files.probeContentType(file));
+					nomes.add(file.toString());
+				}
+				return super.visitFile(file, attrs);
+			}
+		});
+		System.out.println("================================================");
+		for (int i=0; i<tipos.size(); i++) {
+			System.out.println(tipos.get(i)+"						| "+nomes.get(i));
+		}
 		
 	}
 }
+
+
